fb92f51ffbc2385461f6c8828f7f8a7416cbb2d5
Sean <xseenyy@gmail.com>
2024-07-28 19:06:04 +0800

    f

---
+++ favicon/script.js
@@ -0,0 +1,19 @@
+const usesDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches || false;
+
+var link = document.createElement('link');
+link.href = `favicon/favicon_light.png`;
+link.rel = "icon"
+link.type = "image/x-icon"
+link.id = "favicon"
+document.head.appendChild(link);
+
+function switchIcon(usesDarkMode) {
+    if (usesDarkMode) { 
+		link.href = 'favicon/favicon_dark.png';
+    } else {
+		link.href = 'favicon/favicon_light.png';
+    }
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener( "change", (e) => switchIcon(e.matches));
+switchIcon(usesDarkMode);
