bd1bb1995992e27d999a9959d5b42f8003d9ce1f
Sean <xseenyy@gmail.com>
2024-08-05 19:54:50 +0900

    fsd

--- xtaiaoLib/favicon/script.js
+++ xtaiaoLib/favicon/script.js
@@ -1,7 +1,7 @@
 const usesDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches || false;
 
 var link = document.createElement('link');
-link.href = `favicon/favicon_light.png`;
+link.href = `xtaiaoLib/favicon/favicon_light.png`;
 link.rel = "icon"
 link.type = "image/x-icon"
 link.id = "favicon"
@@ -9,13 +9,11 @@ document.head.appendChild(link);
 
 function switchIcon(usesDarkMode) {
     if (usesDarkMode) { 
-		link.href = 'favicon/favicon_dark.png';
+		link.href = 'xtaiaoLib/favicon/favicon_dark.png';
     } else {
-		link.href = 'favicon/favicon_light.png';
+		link.href = 'xtaiaoLib/favicon/favicon_light.png';
     }
 }
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener( "change", (e) => switchIcon(e.matches));
 switchIcon(usesDarkMode);
-
-// testing
